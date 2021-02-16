4dc926e45d746527f41566b59a218721e1ebaebf
Flavio Carrara De Capua <flavio@carrara.us>
2021-02-16 12:49:28 -0300

    simple pthread

--- philo_one/game.c
+++ philo_one/game.c
@@ -6,15 +6,38 @@
 /*   By: fde-capu <fde-capu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 10:51:45 by fde-capu          #+#    #+#             */
-/*   Updated: 2021/02/16 11:00:52 by fde-capu         ###   ########.fr       */
+/*   Updated: 2021/02/16 12:49:02 by fde-capu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
+void	*round_play(void *arg)
+{
+	(void)arg;
+	return NULL;
+}
+
 void	game_start(void)
 {
+	int			error;
+	int			id;
+	pthread_t	tid[g_philo_limit + 1];
+
 	game_intro();
+	id = 0;
+	while (++id <= g_philo_limit)
+	{
+		error = pthread_create(&(tid[id]), NULL, &round_play, NULL);
+		if (error)
+			exit(-1);
+	}
+	id = 0;
+	while (++id <= g_philo_limit)
+	{
+		pthread_join(tid[id], NULL);
+	}
+	game_outro();
 	return ;
 }
 
@@ -30,6 +53,13 @@ void	game_intro(void)
 	fflush(stdout);
 	sleep(1);
 	printf("\b\bnow! ");
+	fflush(stdout);
+	return ;
+}
+
+void	game_outro(void)
+{
+	printf("\n\nGAME OVER\n\n");
 	fflush(stdout);
 	sleep(1);
 	return ;
--- philo_one/header.h
+++ philo_one/header.h
@@ -6,7 +6,7 @@
 /*   By: fde-capu <fde-capu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 13:25:33 by fde-capu          #+#    #+#             */
-/*   Updated: 2021/02/16 11:00:39 by fde-capu         ###   ########.fr       */
+/*   Updated: 2021/02/16 12:46:18 by fde-capu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,5 +74,7 @@ void		philo_destroy(t_philo *p);
 const char	*fork_string(int *fork);
 void		game_start(void);
 void		game_intro(void);
+void		game_outro(void);
+void		*round_play(void *arg);
 
 #endif
