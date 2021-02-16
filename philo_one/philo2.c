dc62504c1727658486b4a1933bbc8a8ba7b78c56
Flavio Carrara De Capua <flavio@carrara.us>
2021-02-16 10:26:33 -0300

    philo2

--- philo_one/philo2.c
+++ philo_one/philo2.c
@@ -1,70 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.c                                            :+:      :+:    :+:   */
+/*   philo2.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fde-capu <fde-capu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/16 07:39:53 by fde-capu          #+#    #+#             */
-/*   Updated: 2021/02/16 10:25:01 by fde-capu         ###   ########.fr       */
+/*   Created: 2021/02/16 10:24:30 by fde-capu          #+#    #+#             */
+/*   Updated: 2021/02/16 10:25:45 by fde-capu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-t_philo	*philo_init(int id)
+void	philo_init_all(void)
 {
-	t_philo	*p;
-	
-	p = calloc(1, sizeof(t_philo));
-	p->id = id;
-	p->state = STATE_DIE;
-	return (p);
-}
-
-void	philo_destroy_all(t_philo *p)
-{
-	if (p->l->id == 1)
-	{
-		free(p);
-		return ;
-	}
-	philo_destroy_all(p->l);
-	free(p);
-	return ;
-}
-
-t_philo	*philo_init_chain(int id)
-{
-	t_philo	*p;
-
-	p = philo_init(id);
-	if (id == 1)
-		g_philo_one = p;
-	if (id == g_philo_limit)
-	{
-		p->l = g_philo_one;
-		return (p);
-	}
-	else
-		p->l = philo_init_chain(id + 1);
-	return (p);
-}
-
-void	philo_log_all(void)
-{
-	int	id;
-
-	id = 0;
-	while (++id <= g_philo_limit)
-		philo_log(id);
-	return ;
-}
-
-void	philo_log(int id)
-{
-	printf("Philosopher %d\t", id);
-	printf("%s\n", state_string(get_philo(id)->state));
+	philo_init_chain(1);
+	philo_link_r();
 	return ;
 }
 
