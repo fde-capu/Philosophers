ca02b0a9e010769f0302848252a895f4519482cf
Flavio Carrara De Capua <flavio@carrara.us>
2021-03-02 16:03:46 -0300

    --

--- philo_one/clock.c
+++ philo_one/clock.c
@@ -6,7 +6,7 @@
 /*   By: fde-capu <fde-capu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/01 09:13:23 by fde-capu          #+#    #+#             */
-/*   Updated: 2021/03/02 15:40:02 by fde-capu         ###   ########.fr       */
+/*   Updated: 2021/03/02 16:03:40 by fde-capu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ void	*fn_clock(void *arg)
 			exit(-1);
 		g_clock = (((now.tv_sec * 1000) + now.tv_usec / 1000) - \
 			((g_init_time.tv_sec * 1000) + g_init_time.tv_usec / 1000));
-		usleep(50);
+		usleep(1000);
 	}
 	return (0);
 }
